4af3d135374ba57f432bd624a4394c9d5fa506ff
kadafi abdou <kadafi.abdou@outlook.com>
2023-07-28 02:18:22 +0200

    add Comment on any files

--- src/app/pages/shared/shared/shared.module.ts
+++ src/app/pages/shared/shared/shared.module.ts
@@ -1,21 +1,37 @@
+// Importation de NgModule d'Angular, qui est nécessaire pour créer un module
 import { NgModule } from '@angular/core';
+
+// Importation de CommonModule, qui contient des fonctionnalités courantes pour les composants
 import { CommonModule } from '@angular/common';
+
+// Importation de NgbModule, qui est un ensemble de composants Bootstrap pour Angular
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Importation de ContactRequestService, qui est probablement un service que vous avez écrit pour gérer les demandes de contact
 import { ContactRequestService } from 'src/app/services/contact-request.service';
 
+// On crée un tableau des modules que nous voulons importer et réexporter
 const MODULES = [
-  CommonModule,
-  NgbModule, // Module pour les fonctionnalités de la bibliothèque ng-bootstrap
+  CommonModule, // Directives de base d'Angular comme ngIf, ngFor, etc.
+  NgbModule, // Composants Bootstrap pour Angular
 ]
 
+// Déclaration de notre module
 @NgModule({
-  imports: [    
+  imports: [
+    // On importe les modules déclarés ci-dessus
     ...MODULES
   ],
-  declarations: [],
+  declarations: [], // Ici, nous pouvons déclarer les composants/directives spécifiques à ce module (actuellement vide)
   exports: [
+    // On réexporte les modules pour que les modules qui importent SharedModule aient accès à CommonModule et NgbModule
     ...MODULES
   ],
-  providers: [ContactRequestService]
+  providers: [
+    // On déclare que ContactRequestService est un fournisseur pour ce module,
+    // ce qui signifie que tous les composants de ce module partageront la même instance de ce service
+    ContactRequestService
+  ]
 })
+// On exporte le module pour qu'il puisse être importé par d'autres modules
 export class SharedModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+// Importation de la fonction 'platformBrowserDynamic' depuis la bibliothèque Angular
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+// Importation de votre module racine (AppModule) de l'application
 import { AppModule } from './app/app.module';
 
-
+// La méthode 'bootstrapModule' initialise l'application en utilisant le module racine
+// (dans ce cas, AppModule).
+// 'platformBrowserDynamic().bootstrapModule(AppModule)' indique à Angular de démarrer l'application avec 'AppModule' comme module racine.
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    // En cas d'erreur lors du démarrage de l'application, l'erreur sera affichée dans la console
+    .catch(err => console.error(err));
